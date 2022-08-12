71d3ea3b41b5309a7d1d187269cdc114509bb4d4
Mark Sayers <marksayers46@icloud.com>
2022-08-11 19:03:40 -0500

    playing with the fetch api

---
+++ src/Table.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Row from './Row';
+
+const Table = ({ items }) => {
+    return (
+        <div className='table-container'>
+            <tbody>
+                {items.map(item => (
+                   <Row key={item.id} item={item} /> 
+                ))}
+            </tbody>
+        </div>
+    )
+}
+
+export default Table;
